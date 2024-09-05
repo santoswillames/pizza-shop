2b1254e81271655653f4333ba69b84d16daf8b97
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2024-09-05 16:26:46 -0300

    feat: add profile api in component

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
+  VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 })
 
 // com o parse a gente valida que o env tenha a mesma estrutura do schema
